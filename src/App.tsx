9e2316a5c93956396eb7b359ce9213402064f629
Valera-Filimonov <VOleFilimonov@sberbank.ru>
2021-08-03 17:02:38 +0500

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import './App.css';
+import styles from './app.module.css';
 import * as React from 'react';
+import video from "./image/video.png";
+import scale from "./image/scale.png"
+import qr from "./image/qr1.png";
 
 const App = (): JSX.Element => {
-  return (
-    <>
-      Hello world Fruw371swT
-    </>
-  );
-}
+    const [panelActive, setPanelActive] = React.useState(true)
+
+    const showPanel = () => {
+        setPanelActive(!panelActive);
+    };
+
+    return (
+        <section className={styles.banner} style={{backgroundImage: `url(${panelActive ? video : scale})`}}>
+            {
+                panelActive && (
+                    <div className={styles.frame}>
+                        <p className={styles.heading_frame}
+                           style={{width: 255}}>ИСПОЛНИТЕ МЕЧТУ ВАШЕГО <br/>МАЛЫША! <br/>ПОДАРИТЕ ЕМУ СОБАКУ!</p>
+                        <img className={styles.qr_code} src={qr}/>
+                        <p className={styles.text_invitation}
+                           style={{width: 258}}>Сканируйте QR-код <br/>или нажмите ОК</p>
+                        <button className={styles.button} onClick={showPanel}>ОК</button>
+                    </div>
+                )
+            }
+        </section>
+    );
+};
 
 export default App;
